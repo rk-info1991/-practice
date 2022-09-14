5f7faaa11b1fa2323a9bf1dbc836cc9f917646dd
Роман Богомолов <rk-info@mail.ru>
2022-09-14 20:01:58 +0300

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+"use strict";
+
+
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -6,11 +10,28 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", ""),
-    c = prompt("Один из последних просмотренных фильмов?", ""),
-    d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", '');
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+    } else {
+        console.log("error");
+        i--;
+    }
+}
+if (personalMovieDB.count < 10) {
+    console.log("Просмотренно довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.movies < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("произошла ошибка");
+}
+
+
 console.log(personalMovieDB);
